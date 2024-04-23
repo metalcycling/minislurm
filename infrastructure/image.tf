c017642c58760fc9c328bf80f49bf6dc4635dbe3
Pedro D. Bello-Maldonado <metalcycling@gmail.com>
2024-04-23 14:19:04 -0400

    Output of 'terraform fmt'

--- infrastructure/image.tf
+++ infrastructure/image.tf
@@ -1,20 +1,20 @@
 resource "null_resource" "create_authorized_keys" {
   provisioner "local-exec" {
     command = "cat ~/.ssh/id_rsa.pub > authorized_keys"
-    when = create
+    when    = create
   }
   provisioner "local-exec" {
     command = "rm -f authorized_keys"
-    when = destroy
+    when    = destroy
   }
 }
 
 resource "docker_image" "minislurm_blank" {
-  name = "minislurm-blank"
+  name       = "minislurm-blank"
   depends_on = [null_resource.create_authorized_keys]
   build {
-    context = "."
-    dockerfile = "Dockerfile"
+    context      = "."
+    dockerfile   = "Dockerfile"
     force_remove = true
   }
 }
